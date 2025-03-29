e44b829140b34985e9351dfdc68229cf95225c8a
Rana Muneeb <ranamuneeb184@gmail.com>
2025-03-29 23:13:56 +0500

    prievew added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/screen/homepage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( 
+    DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(),
+  ),);
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,8 +16,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
+      // ignore: deprecated_member_use
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      //theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home:  Homepage(),
-      theme: ThemeData.dark(),
+      //theme: ThemeData.dark(),
       title: "Todo App",
       debugShowCheckedModeBanner: false,
     
